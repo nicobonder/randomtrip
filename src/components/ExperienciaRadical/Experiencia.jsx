ab5f79043d8d51809bc509ef5d90a79b53cd2625
nicobonder <nicobonder@hotmail.com>
2023-07-16 18:27:38 -0300

    Uploading last changes

--- src/components/ExperienciaRadical/Experiencia.jsx
+++ src/components/ExperienciaRadical/Experiencia.jsx
@@ -5,28 +5,28 @@ import video from './VideoWorld.mp4';
 import { Country, State, City }  from 'country-state-city';
 
 const Experiencia = () => {
-    const [map, setMap] = useState(false);
-    const [lon, setLon] = useState(0);
-    const [lat, setLat] = useState(0);
-    const [CityName, setCityName] = useState('');
+  const [map, setMap] = useState(false);
+  const [lon, setLon] = useState(0);
+  const [lat, setLat] = useState(0);
+  const [CityName, setCityName] = useState('');
 
-      const cityList = City.getAllCities() 
-  
-      const randomIndex = Math.floor(Math.random() * cityList.length)
-      console.log('randomIndex', randomIndex)
-  
-      const randomCity = cityList[randomIndex]
-      const latitude = randomCity.latitude
-      const longitude = randomCity.longitude
-      const name = randomCity.name   
-  
-      const handleMap = async () => {
-              setLon(longitude); // Actualiza el valor de lon
-              setLat(latitude); // Actualiza el valor de lat
-              setCityName(name)
-              setMap(true); // Activa el mapa
-          };
-        
+  const cityList = City.getAllCities() 
+
+  const randomIndex = Math.floor(Math.random() * cityList.length)
+  console.log('randomIndex', randomIndex)
+
+  const randomCity = cityList[randomIndex]
+  const latitude = randomCity.latitude
+  const longitude = randomCity.longitude
+  const name = randomCity.name   
+
+  const handleMap = async () => {
+      setLon(longitude); // Actualiza el valor de lon
+      setLat(latitude); // Actualiza el valor de lat
+      setCityName(name)
+      setMap(true); // Activa el mapa
+  };
+      
     return (
       <div className='experienceSection'>
         <h1 className='destinationTitle'>Bienvenido a una experiencia radical</h1>
